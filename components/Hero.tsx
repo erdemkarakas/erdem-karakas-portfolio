b6f613defe0eae50b07e411661fff44088415588
erdemkarakas <erdemkarakas26@gmail.com>
2025-02-05 20:45:42 +0300

    fix position

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -273,7 +273,7 @@ const Hero = () => {
                     repeat: Infinity,
                     repeatType: "reverse",
                 }}
-                className="absolute bottom-8 left-1/2 transform -translate-x-1/2"
+                className="absolute bottom-8 left-1/2 -translate-x-1/2 flex justify-center items-center w-full"
             >
                 <div className="w-6 h-10 border-2 border-blue-500/30 rounded-full flex justify-center">
                     <div className="w-1 h-2 bg-blue-500/30 rounded-full mt-2" />
